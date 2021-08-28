27186f05d95516fb6b90819e9d6beb5ce0bbbef8
Ethan Betts-Colefax <ethan.bettscolefax@my.jcu.edu.au>
2021-08-28 15:20:19 +1000

    refactor password check program to include functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,7 +1,21 @@
 MIN_LENGTH = 10
-password = input("Enter password (min. 10 characters): ")
-while len(password) < MIN_LENGTH:
-    print("Password is too short")
-    password = input("Enter password (min. 10 characters): ")
-print("*" * len(password))
 
+
+def main():
+    valid_password = get_password()
+    display_password(valid_password)
+
+
+def get_password():
+    password = input("Enter password (min. 10 characters): ")
+    while len(password) < MIN_LENGTH:
+        print("Password is too short")
+        password = input("Enter password (min. 10 characters): ")
+    return password
+
+
+def display_password(password):
+    print("*" * len(password))
+
+
+main()
