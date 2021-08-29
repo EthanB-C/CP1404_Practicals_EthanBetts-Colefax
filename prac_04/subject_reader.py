866760266998dcaba631888bfacff0e4df59fbaf
Ethan Betts-Colefax <ethan.bettscolefax@my.jcu.edu.au>
2021-08-29 20:29:08 +1000

    modify subject reader program

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,12 +8,14 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
+    display_subject_details(data)
     print(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data_lists = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,7 +25,14 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        data_lists.append(parts)
     input_file.close()
+    return data_lists
+
+
+def display_subject_details(subject_data_list):
+    for subject_details in subject_data_list:
+        print("{} is taught by {} and has {} students".format(subject_details[0], subject_details[1], subject_details[2]))
 
 
 main()
