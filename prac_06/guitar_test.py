e7b795e8004f8b6276cbf9a7421dde37ab815b9c
Ethan Betts-Colefax <ethan.bettscolefax@my.jcu.edu.au>
2021-09-12 13:16:55 +1000

    finish guitar_test.py program

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -4,11 +4,13 @@ from prac_06.guitar import Guitar
 
 guitar1 = [Guitar("Gibson L-5 CES", 1922, 16035.40), 99, True]
 guitar2 = [Guitar("Another Guitar", 2013), 8, False]
+guitars = [guitar1, guitar2]
 
 
 def main():
-    print("{} get_age() - Expected {}. Got {}".format(guitar1[0].name, guitar1[1], guitar1[0].get_age()))
-    print("{} is_vintage() - Expected {}. Got {}".format(guitar1[0].name, guitar1[2], guitar1[0].is_vintage()))
+    for guitar in guitars:
+        print("{} get_age() - Expected {}. Got {}".format(guitar[0].name, guitar[1], guitar[0].get_age()))
+        print("{} is_vintage() - Expected {}. Got {}".format(guitar[0].name, guitar[2], guitar[0].is_vintage()))
 
 
 main()
