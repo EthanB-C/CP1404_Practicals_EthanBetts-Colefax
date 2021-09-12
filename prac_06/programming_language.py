6ec493e13e2517805e71a1e39f8cc9e36db42885
Ethan Betts-Colefax <ethan.bettscolefax@my.jcu.edu.au>
2021-09-12 12:25:53 +1000

    add programming language class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,14 @@
+"""Programming Language Program"""
+
+
+class ProgrammingLanguage:
+    def __init__(self, typing, reflection, year):
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        if self.typing == 'Dynamic':
+            return True
+        else:
+            return False
