78b22d5e1a6577c294fdc880b866124a86a00bcd
Ethan Betts-Colefax <ethan.bettscolefax@my.jcu.edu.au>
2021-09-26 20:32:06 +1000

    add formatting to the convert_miles_km.py

--- prac_07/KivyDemos-master/convert_miles_km.py
+++ prac_07/KivyDemos-master/convert_miles_km.py
@@ -1,18 +1,26 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.core.window import Window
 from kivy.properties import StringProperty
 
 
 class ConvertMilesToKM(App):
+    message = StringProperty()
+
     def build(self):
+        Window.size = (600, 300)
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def convert_miles_to_kilometres(self, user_input):
-        miles = int(user_input)
+    def convert_miles_to_kilometres(self, input_miles):
+        miles = int(input_miles)
         kilometres = miles * 1.609
-        self.root.ids.output_text.text = str(kilometres)
+        self.message = str(kilometres)
+
+    def handle_increment(self, increment):
+        new_input_miles = int(self.root.ids.user_input.text) + increment
+        self.root.ids.user_input.text = str(new_input_miles)
 
 
 ConvertMilesToKM().run()
